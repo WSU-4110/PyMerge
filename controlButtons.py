a595dbc6aace7a08024601514d38eb3f874c2086
jmtoniolo <gd0911@wayne.edu>
2019-09-25 14:08:53 -0400

    mainWindow merged with master branch
    mainWindow merged with master branch. you can now start the program
    with PyMerge.py with/without command line params.

---
+++ controlButtons.py
@@ -0,0 +1,30 @@
+"""
+Main Window
+"""
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui
+#from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QGridLayout, QPushButton, QGroupBox, QDialog, QVBoxLayout
+
+
+
+class controlButtons(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 200, 200)
+        self.buttonLayout()
+                
+    def buttonLayout(self):        
+        grid = QGridLayout()
+        self.setLayout(grid)
+        
+        mergeLBut = QPushButton("Merge Left")
+        grid.addWidget(mergeLBut, 0, 0)
+        mergeRBut = QPushButton("Merge Right")
+        grid.addWidget(mergeRBut, 0, 3)
+        nextDBut =  QPushButton("Next Difference")
+        grid.addWidget(nextDBut, 0, 1)
+        prevDBut =  QPushButton("Previous Difference")
+        grid.addWidget(prevDBut, 0, 2)
+        
--- mainWindow.py
+++ mainWindow.py
@@ -8,33 +8,7 @@ Main Window
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
-#from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QGridLayout, QPushButton, QGroupBox, QDialog, QVBoxLayout
-
-
-
-class controlButtons(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(200, 200, 200, 200)
-        self.buttonLayout()
-                
-    def buttonLayout(self):        
-        grid = QGridLayout()
-        self.setLayout(grid)
-        
-        mergeLBut = QPushButton("Merge Left")
-        grid.addWidget(mergeLBut, 0, 0)
-        mergeRBut = QPushButton("Merge Right")
-        grid.addWidget(mergeRBut, 0, 3)
-        nextDBut =  QPushButton("Next Difference")
-        grid.addWidget(nextDBut, 0, 1)
-        prevDBut =  QPushButton("Previous Difference")
-        grid.addWidget(prevDBut, 0, 2)
-        
-        
-        
-        
-
+import controlButtons
 
 class mainWindow(QMainWindow):    
     def __init__(self, fileA=0, fileB=0):
@@ -43,8 +17,8 @@ class mainWindow(QMainWindow):
 
         layout = QGridLayout()
 
-        layout.addWidget(controlButtons(), 0, 0)
-        layout.addWidget(controlButtons(), 1, 0)
+        layout.addWidget(controlButtons.controlButtons(), 0, 0)
+        layout.addWidget(controlButtons.controlButtons(), 1, 0)
 
         widget = QWidget()
         widget.setLayout(layout)

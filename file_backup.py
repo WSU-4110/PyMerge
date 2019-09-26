b3b310d4b7f0ac8f7f42d06f205cf6925accb185
Malcolm Hall <malcolmrainehall@gmail.com>
2019-09-26 18:36:42 -0400

    Added comments and type hints.

--- file_backup.py
+++ file_backup.py
@@ -11,10 +11,16 @@ import pickle
 
 class Backup(object):
     def __init__(self, hash_type="SHA256", block_size=65535):
+        """
+        Initialize the Backup class
+        :param hash_type: Type of hash function to use. Defaults to SHA256
+        :param block_size: Size of block to be read from file when calculating hash
+        """
         self.BLOCK_SIZE: int = block_size
         self.hash_type: str = hash_type
         self.hash_func = hashlib.sha256()  # Default to SHA256
 
+        # Choose the correct hash function
         if self.hash_type == "SHA224":
             self.hash_func = hashlib.sha224
         if self.hash_type == "SHA256":
@@ -25,12 +31,21 @@ class Backup(object):
             self.hash_func = hashlib.md5
 
     @staticmethod
-    def format_datetime():
+    def format_datetime() -> str:
+        """
+        Format datetime as string with ':' and ' ' removed.
+        :return: string containing datetime values
+        """
         datetime_str = str(datetime.datetime.now())
         datetime_str = datetime_str.replace(":", "").replace(" ", "_").replace("-", "")
         return datetime_str
 
-    def create_meta_file(self, file: str):
+    def create_meta_file(self, file: str) -> str:
+        """
+        Create a metadata file for use as comparison against unarchived file data
+        :param file: file to be archived
+        :return: string containing absolute path to metadata file
+        """
         file_name = file.replace('\\', "/").split("/")[-1]
         meta_info: dict = {
             "NAME": file_name,
@@ -50,8 +65,6 @@ class Backup(object):
         :param file: file to get hash for
         :return: hex digest version of hash
         """
-
-
         with open(file, 'rb') as in_file:
             file_buf = in_file.read(self.BLOCK_SIZE)
 
@@ -77,15 +90,27 @@ class Backup(object):
         :param file: file to be backed up
         :return: absolute path to backup
         """
+        # Create hash file to use for integrity checks during backup retrieval
         hash_file = f"{self.hash_type}.txt"
+
+        # Create meta data file to use as comparison when backup is retrieved
         meta_file = self.create_meta_file(file)
+
         with open(hash_file, 'w+') as temp:
             temp.write(str(self.get_hash(file)))
 
+        # Zip the files together
         with zipfile.ZipFile(f"{file}.bak", 'w', zipfile.ZIP_DEFLATED) as backup:
             backup.write(file)
             backup.write(hash_file)
+            backup.write(meta_file)
 
+        try:
+            # Remove the created hash and meta data files after they are archived
+            os.remove(hash_file)
+            os.remove(meta_file)
+        except FileNotFoundError:
+            pass
         return os.path.abspath(f"{file}.bak")
 
     def retrieve_backup(self, backup_file, file_name):
@@ -93,26 +118,33 @@ class Backup(object):
         Function to retrieve the backup file from the zip archive. Checks the hash value for integrity
         :param backup_file: backup file to extract
         :param file_name: file to be backed up
-        :return:
+        :return: No return value
         """
+
+        # Extract the archive contents
         with zipfile.ZipFile(backup_file) as myzip:
             myzip.extract(file_name)
             myzip.extract(f"{self.hash_type}.txt")
+            myzip.extract(f".meta.{file_name}.dat")
 
+        # Read the hash file contents
         with open(f"{self.hash_type}.txt", 'r') as hash_file:
             hash_string = hash_file.read().strip('\n')
 
+        # Check the integrity of the file against the included hash
         if not self.check_hash(backup_file.strip(".bak"), hash_string):
             os.remove(file_name)
-        os.remove(f"{self.hash_type}.txt")
 
+        # Remove unecessary archive files
+        os.remove(f"{self.hash_type}.txt")
+        os.remove(f".meta.{file_name}.dat")
 
 
 def test():
     test_class = Backup()
-    backup = test_class.create_backup("file2.c")
-    os.remove("file2.c")
+    #backup = test_class.create_backup("file2.c")
+    #os.remove("file2.c")
     test_class.retrieve_backup("file2.c.bak", "file2.c")
-    #test_class.create_meta_file("file2.c")
+    # test_class.create_meta_file("file2.c")
 
-test()
+#test()

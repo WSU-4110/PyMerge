ec09beea0ce220dfc2e1a1db6ede4a02b0689b10
Malcolm Hall <malcolmrainehall@gmail.com>
2019-09-26 18:48:24 -0400

    Added comments.

--- gui_config.py
+++ gui_config.py
@@ -4,6 +4,9 @@ Configurations and globals for GUI
 
 from PyQt5 import QtGui
 
+"""
+Icon paths 
+"""
 ICONS = {
     "MERGE_RIGHT": "icons/left-arrow.png",
     "MERGE_LEFT": "icons/right-arrow.png",
@@ -12,14 +15,25 @@ ICONS = {
     "ADD_FILE": "icons/add.png",
 }
 
+
+"""
+Colors to be used for all styling the GUI. 
+Each dictionary object should be an instance of the QColor class
+"""
 COLORS = {
     "LINE_DIFF": QtGui.QColor(255, 150, 150),
     "LINE_MERGE": QtGui.QColor(219, 235, 255),
     "PAD_SPACE": QtGui.QColor(240, 240, 240),
-    "DEFAULT": QtGui.QColor(255, 255, 255)
+    "DEFAULT": QtGui.QColor(255, 255, 255),
+    "TBL_HEADER_DEFAULT_BACKGROUND": QtGui.QColor(179, 179, 179),
+    "TBL_HEADER_DEFAULT_FOREGROUND": QtGui.QColor(0, 0, 0)
+
 }
 
+"""
+Fonts to be used throughout the GUI
+"""
 FONTS = {
-    "TBL_HEADER_DEFAULT": QtGui.QFont('Open Sans Bold', weight=QtGui.QFont.Bold),
+    "TBL_HEADER_DEFAULT": QtGui.QFont('Open Sans Bold', weight=QtGui.QFont.Bold, pointSize=12),
     "TOOLBAR_DEFAULT": QtGui.QFont('Open Sans Bold', weight=QtGui.QFont.Bold)
 }

8730973a6de098b8659f96777e082329e1b69f62
Malcolm Hall <malcolmrainehall@gmail.com>
2019-09-29 15:17:19 -0400

    Added new colors.

--- gui_config.py
+++ gui_config.py
@@ -2,7 +2,7 @@
 Configurations and globals for GUI
 """
 
-from PyQt5 import QtGui
+from PyQt5.QtGui import QFont, QColor
 
 """
 Icon paths 
@@ -13,6 +13,8 @@ ICONS = {
     "UNDO": "icons/undo-arrow.png",
     "REDO": "icons/redo-arrow.png",
     "ADD_FILE": "icons/add.png",
+    "PREV_DIFF": "icons/up-arrow.png",
+    "NEXT_DIFF": "icons/down-arrow.png",
 }
 
 
@@ -21,19 +23,19 @@ Colors to be used for all styling the GUI.
 Each dictionary object should be an instance of the QColor class
 """
 COLORS = {
-    "LINE_DIFF": QtGui.QColor(255, 150, 150),
-    "LINE_MERGE": QtGui.QColor(219, 235, 255),
-    "PAD_SPACE": QtGui.QColor(240, 240, 240),
-    "DEFAULT": QtGui.QColor(255, 255, 255),
-    "TBL_HEADER_DEFAULT_BACKGROUND": QtGui.QColor(179, 179, 179),
-    "TBL_HEADER_DEFAULT_FOREGROUND": QtGui.QColor(0, 0, 0)
-
+    "LINE_DIFF": QColor(255, 150, 150),
+    "LINE_MERGE": QColor(219, 235, 255),
+    "PAD_SPACE": QColor(240, 240, 240),
+    "DEFAULT": QColor(255, 255, 255),
+    "TBL_HEADER_DEFAULT_BACKGROUND": QColor(179, 179, 179),
+    "TBL_HEADER_DEFAULT_FOREGROUND": QColor(0, 0, 0),
+    "TBL_LINE_COL_DEFAULT_BACKGROUND": QColor(242, 242, 242)
 }
 
 """
 Fonts to be used throughout the GUI
 """
 FONTS = {
-    "TBL_HEADER_DEFAULT": QtGui.QFont('Open Sans Bold', weight=QtGui.QFont.Bold, pointSize=12),
-    "TOOLBAR_DEFAULT": QtGui.QFont('Open Sans Bold', weight=QtGui.QFont.Bold)
+    "TBL_HEADER_DEFAULT": QFont("Open Sans Bold", weight=QFont.Bold, pointSize=12),
+    "TOOLBAR_DEFAULT": QFont("Open Sans Bold", weight=QFont.Bold),
 }

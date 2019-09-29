b1897f3507525469d91db43f6e118b4198608656
Malcolm Hall <malcolmrainehall@gmail.com>
2019-09-29 14:14:57 -0400

    Fixed issue where invalid path error was shown when program was invoked with no file arguments.

--- PyMerge.py
+++ PyMerge.py
@@ -7,54 +7,45 @@ algorithms in this file. It will only call the main GUI and application function
 
 import sys
 import os
-
 # import FileCompareTable
 import mainWindow
 
 
 class PyMergeCLI(object):
-    error_msgs = {
-        "FILE_CNT": "Exactly two files must be specified to compare/merge.",
-        "FILE_DNE": "File does not exist: ",
-    }
-
     def __init__(self, *args):
         self.options = self.sanitize(args[0][1:])
         self.cli()
 
     def cli(self):
-        left_file = ""
-        right_file = ""
+        left_file: str = ""
+        right_file: str = ""
+        opt_length = len(self.options)
 
-        if len(self.options) == 0:
+        if opt_length == 0:
             self.invoke_application(left_file, right_file)
             return
-        elif len(self.options) == 2 and self.check_paths(
-            self.options[0], self.options[1]
-        ):
-            left_file = self.options[0]
-            right_file = self.options[1]
-        elif (
-            len(self.options) == 3
-            and self.options[0] == "--file"
-            and self.check_paths(self.options[1], self.options[2])
-        ):
+        elif opt_length == 1 and self.options[0] == "--help":
+            if self.options[0] == "--help":
+                self.help_func()
+            elif self.options[0] == "--about":
+                self.about_func()
+        elif opt_length == 2:
+            if self.check_paths(self.options[0], self.options[1]):
+                left_file = self.options[0]
+                right_file = self.options[1]
+            elif self.options[0] == "--file":
+                print("Error: 2 files required for comparison.")
+        elif opt_length == 3 and self.options[0] == "--file" and self.check_paths(self.options[1], self.options[2]):
             left_file = self.options[1]
             right_file = self.options[2]
-        elif (
-            len(self.options) == 4
-            and self.options[0] == "--file"
-            and self.options[2] == "--file"
-            and self.check_paths(self.options[1], self.options[3])
-        ):
+        elif opt_length == 4 and \
+                self.options[0] == "--file" and \
+                self.options[2] == "--file" and \
+                self.check_paths(self.options[1], self.options[3]):
             left_file = self.options[1]
             right_file = self.options[3]
-        elif len(self.options) == 1 and self.options[0] == "--help":
-            self.help_func()
-        elif len(self.options) == 1 and self.options[0] == "--about":
-            self.about_func()
         else:
-            print("Invalid options. Type '--help' for information.")
+            print("Error: Invalid options. Type '--help' for information.")
             return
 
         self.invoke_application(left_file, right_file)
@@ -82,62 +73,47 @@ PyMerge
     def sanitize(options: list or set) -> list or set:
         sanitized: list = []
         file_options: set = {"--f", "-file", "-f"}
-        help_options: set = {
-            "--h",
-            "-h",
-            "--he",
-            "-he",
-            "--hel",
-            "-hel",
-            "--help",
-            "-help",
-        }
-        about_options: set = {
-            "--about",
-            "-about",
-            "--abot",
-            "-abot",
-            "--abut",
-            "-abut",
-            "--abt",
-            "-abt",
-            "--info",
-            "-info",
-        }
+        help_options: set = {"--h", "-h", "--he", "-he", "--hel", "-hel", "--help", "-help"}
+        about_options: set = {"--about", "-about", "--abot", "-abot", "--abut", "-abut", "--abt", "-abt", "--info",
+                              "-info"}
 
         for n in range(len(options)):
             options[n] = str(options[n].replace(" ", ""))
+            option_lower: str = options[n].lower()
 
-            if options[n] == " " or options[n] == "":
+            if option_lower == " " or options[n] == "":
                 continue
-            elif options[n].lower() in file_options:
+            elif option_lower in file_options:
                 sanitized.append("--file")
-            elif options[n].lower() in help_options:
+            elif option_lower in help_options:
                 sanitized.append("--help")
-            elif options[n].lower() in about_options:
+            elif option_lower in about_options:
                 sanitized.append("--about")
             else:
                 sanitized.append(options[n])
 
         return sanitized
 
-    @staticmethod
-    def invoke_application(file1, file2):
-        # """Invoke the main application here"""
-        # if len(sys.argv) == 4:
-        #     mainWindow.startMain(file1, file2)
-        # else:
-        #     mainWindow.startMain()
+    def invoke_application(self, file1: str, file2: str):
+        """Invoke the main application here"""
+        if file1 != "" or file2 != "":
+            if self.check_paths(file1, file2):
+                mainWindow.startMain(file1, file2)
+        else:
+            mainWindow.startMain()
         print(file1, file2)
 
     @staticmethod
     def check_paths(*args):
         for arg in args:
-            if not os.path.exists(arg) or not os.path.isfile(arg):
-                print("Invalid file path: ", arg)
+            try:
+                if not os.path.exists(arg) or not os.path.isfile(arg):
+                    print("Invalid file path: ", arg)
+                    return False
+            except (FileNotFoundError, FileExistsError):
                 return False
         return True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     PyMergeCLI(sys.argv)

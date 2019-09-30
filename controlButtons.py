6d0856cfa1e899036b29199d90d8aa3628853d39
Malcolm Hall <malcolmrainehall@gmail.com>
2019-09-30 11:44:00 -0400

    Added undo and redo buttons, changed tool button sizes, changed button layout.

--- controlButtons.py
+++ controlButtons.py
@@ -3,7 +3,7 @@ Widget to contain the buttons/control panel for the merge tool.
 """
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
-
+from PyQt5 import QtCore
 import gui_config
 
 
@@ -12,30 +12,38 @@ class controlButtons(QWidget):
         super().__init__()
         self.setGeometry(200, 200, 200, 200)
         self.buttonLayout()
-                
-    def buttonLayout(self):        
+
+
+
+    def buttonLayout(self):
         grid = QGridLayout()
         self.setLayout(grid)
-                
-        merge_left_button = QPushButton()
-        merge_left_button.resize( 100, 40)
-        icon = QtGui.QIcon(gui_config.ICONS["MERGE_LEFT"])
+
+        merge_left_button = QToolButton()
+        merge_left_button.setFixedSize(60, 60)
+        icon = QtGui.QIcon(gui_config.ICONS["UNDO"])
         merge_left_button.setIcon(icon)
-        
+        merge_left_button.setIconSize(QtCore.QSize(25, 25))
         grid.addWidget(merge_left_button, 0, 0)
 
-        merge_right_button = QPushButton()
-        icon = QtGui.QIcon(gui_config.ICONS["MERGE_RIGHT"])
+        merge_right_button = QToolButton()
+        merge_right_button.setFixedSize(60, 60)
+        icon = QtGui.QIcon(gui_config.ICONS["REDO"])
         merge_right_button.setIcon(icon)
-        grid.addWidget(merge_right_button, 0, 2)
-        
-        next_diff_button = QPushButton()
+        merge_right_button.setIconSize(QtCore.QSize(25, 25))
+        grid.addWidget(merge_right_button, 0, 3)
+
+        next_diff_button = QToolButton()
+        next_diff_button.setFixedSize(60, 60)
         icon = QtGui.QIcon(gui_config.ICONS["NEXT_DIFF"])
         next_diff_button.setIcon(icon)
+        next_diff_button.setIconSize(QtCore.QSize(25, 25))
         grid.addWidget(next_diff_button, 0, 1)
-        
-        prev_diff_button = QPushButton()
+
+        prev_diff_button = QToolButton()
+        prev_diff_button.setFixedSize(60, 60)
         icon = QtGui.QIcon(gui_config.ICONS["PREV_DIFF"])
         prev_diff_button.setIcon(icon)
-        grid.addWidget(prev_diff_button, 1, 1)
-        
+        prev_diff_button.setIconSize(QtCore.QSize(25, 25))
+        grid.addWidget(prev_diff_button, 0, 2)
+

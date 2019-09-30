024dfbe5c8fcf9ad345b51bee05fad91b1c61e07
saular <saularraffi@test.com>
2019-09-29 20:09:30 -0400

    modify algorithm.py and changeSet.py

--- algorithm.py
+++ algorithm.py
@@ -3,37 +3,45 @@ algorithm class. Must be provided with two files, and two changeSet objects. It 
 the files and populate the ChangeSet Objects
 """
 import pmEnums
-#from pmEnums import CHANGEDENUM
 import changeSet
+import os
 
 class algorithm:
     def __init__(self):
         pass
     
-    def getChangeSets( self, iFileA, iFileB, ochangeSetA, ochangeSetB ):
+    def getChangeSets( self, iFileNameA, iFileNameB, ochangeSetA, ochangeSetB ):
         # file line 1 = stringA
         # ioFileA.attributeStorageVariable = stringA
         
-        f1 = iFileA.readlines()
-        f2 = iFileB.readlines()
+        f1 = open(iFileNameA).readlines()
+        f2 = open(iFileNameB).readlines()
+
+        f1_name, f1_ext = os.path.splitext(iFileNameA)
+        f2_name, f2_ext = os.path.splitext(iFileNameB)
+
+        if f1_ext != f2_ext:
+            print("[-] Incompatible file types", f1_ext, "and", f2_ext)
+            return pmEnums.RESULT.FILEMISMAT
 
         for line in range(len(f1)):
         	if f1[line] == f2[line]:
-        		file1Changes.setChange(line, pmEnums.CHANGEDENUM.SAME, f1[line])
+        		file1Changes.addChange(line, pmEnums.CHANGEDENUM.SAME, f1[line])
         	else:
-        		file1Changes.setChange(line, pmEnums.CHANGEDENUM.CHANGED, f1[line])
+        		file1Changes.addChange(line, pmEnums.CHANGEDENUM.CHANGED, f1[line])
+
 
         return pmEnums.RESULT.NOTIMPL
 
+        # return pmEnums.RESULT.ERROR
+        # return pmEnums.RESULT.GOOD
 
-file1 = open("test_file1.txt", "r")
-file2 = open("test_file2.txt", "r")
 
 file1Changes = changeSet.ChangeSet()
 file2Changes = changeSet.ChangeSet()
 
 alg = algorithm()
-alg.getChangeSets(file1, file2, file1Changes, file2Changes)
+alg.getChangeSets("test_file1.txt", "test_file2.txt", file1Changes, file2Changes)
 
 
 

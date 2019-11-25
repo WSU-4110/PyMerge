4f94f302ab33d52db8bfdac759bae48b3ccca7bf
Malcolm Hall <malcolmrainehall@gmail.com>
2019-11-25 13:41:13 -0500

    Commit for unit tests for methods in file_backup.py

--- test_backup.py
+++ test_backup.py
@@ -13,7 +13,6 @@ class TestInit(TestBackup):
     def test_initial_block_size(self):
         self.assertEqual(self.backup.BLOCK_SIZE, 65535)
         self.assertNotEqual(self.backup.BLOCK_SIZE, 0)
-        self.assertAlmostEqual(self.backup.BLOCK_SIZE, 65534)
 
     def test_initial_hash_type(self):
         self.assertEqual(self.backup.hash_type, "SHA256")
@@ -47,6 +46,7 @@ class TestMethods(TestBackup):
         self.assertNotEqual(self.backup.format_datetime(" "), " ")
 
     def test_check_for_backup_folder(self):
+        self.assertEqual(os.path.exists("backup"), False)
         self.backup.check_for_backup_folder()
         self.assertEqual(os.path.exists("backup"), True)
         os.rmdir("backup")
@@ -60,8 +60,17 @@ class TestMethods(TestBackup):
         self.assertNotEqual(self.backup.get_meta_file_name("test.c"), ".meta.test.dat")
         self.assertNotEqual(self.backup.get_meta_file_name("test.c"), "test.c.dat")
 
-    # def test_get_hash(self):
-    #     self.fail()
+    def test_get_hash(self):
+        open("hash_test.txt", 'w+').close()
+        self.backup.hash_func = self.backup.get_hash_func("SHA256")
+        self.assertEqual(self.backup.get_hash("hash_test.txt"), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+        self.backup.hash_func = self.backup.get_hash_func("MD5")
+        self.assertEqual(self.backup.get_hash("hash_test.txt"), "d41d8cd98f00b204e9800998ecf8427e")
+        self.backup.hash_func = self.backup.get_hash_func("SHA224")
+        self.assertEqual(self.backup.get_hash("hash_test.txt"), "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f")
+        self.backup.hash_func = self.backup.get_hash_func("SHA512")
+        self.assertEqual(self.backup.get_hash("hash_test.txt"), "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")
+        os.remove("hash_test.txt")
 
     def test_get_hash_file_name(self):
         self.assertEqual(self.backup.get_hash_file_name("SHA256"), "SHA256.txt")

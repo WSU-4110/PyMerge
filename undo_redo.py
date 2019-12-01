c19fdec398ec1723d79fd1464377bed0c47ba3cc
Malcolm Hall <malcolmrainehall@gmail.com>
2019-12-01 16:42:50 -0500

    Added license to top of file.

--- undo_redo.py
+++ undo_redo.py
@@ -1,4 +1,28 @@
 """
+###########################################################################
+File:
+Author:
+Description:
+
+
+Copyright (C) 2019
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###########################################################################
+"""
+
+"""
 Undo function can be implemented by keeping a buffer of each state change. That is, each time a change is made, 
 the row state is  copied to a list containing the last n number of row copies. If undo is selected, the 
 undo index is recorded and that copy of the row state is used to replace the current working version. The current working version
